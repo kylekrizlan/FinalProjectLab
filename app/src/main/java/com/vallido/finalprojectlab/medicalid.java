3835c06a0f938960b23326e6e568ab8cdc2532a4
aaronmguiang <43330157+aaronmguiang@users.noreply.github.com>
2018-12-07 14:49:40 +0800

    lab

--- app/src/main/java/com/vallido/finalprojectlab/medicalid.java
+++ app/src/main/java/com/vallido/finalprojectlab/medicalid.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class medicalid extends AppCompatActivity {
+
+    public TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicalid);
+     //   name.setText(findViewById(R.id.name));
     }
+
     public void toMedication(View view) {
         Intent i = new Intent(getApplicationContext(), medication.class);
         startActivity(i);
